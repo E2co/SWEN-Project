008930ff685bd7d104f1eeafca703fe8bf60564c
natecross123 <137192717+natecross123@users.noreply.github.com>
2024-11-27 12:58:41 -0500

    Edit Notif

--- notification.php
+++ notification.php
@@ -14,27 +14,35 @@ if ($conn->connect_error) {
 $sql = "
     SELECT s.id, s.name
     FROM (
-        SELECT sa1.id, sa1.date AS date1, sa2.date AS date2, sa3.date AS date3
+        -- Check for 3 consecutive absences
+        SELECT sa1.student_id, sa1.date AS date1, sa2.date AS date2, sa3.date AS date3
         FROM student_attendance sa1
-        JOIN student_attendance sa2 ON sa1.id = sa2.id AND sa2.date = DATE_ADD(sa1.date, INTERVAL 1 DAY)
-        JOIN student_attendance sa3 ON sa1.id = sa3.id AND sa3.date = DATE_ADD(sa2.date, INTERVAL 1 DAY)
+        JOIN student_attendance sa2 ON sa1.student_id = sa2.student_id AND sa2.date = DATE_ADD(sa1.date, INTERVAL 1 DAY)
+        JOIN student_attendance sa3 ON sa1.student_id = sa3.student_id AND sa3.date = DATE_ADD(sa2.date, INTERVAL 1 DAY)
         WHERE sa1.status = 'absent' AND sa2.status = 'absent' AND sa3.status = 'absent'
-    ) AS consecutive_absences
-    JOIN students s ON s.id = consecutive_absences.id
+        
+        UNION
+        
+        -- Check for 3 consecutive lateness
+        SELECT sa1.student_id, sa1.date AS date1, sa2.date AS date2, sa3.date AS date3
+        FROM student_attendance sa1
+        JOIN student_attendance sa2 ON sa1.student_id = sa2.student_id AND sa2.date = DATE_ADD(sa1.date, INTERVAL 1 DAY)
+        JOIN student_attendance sa3 ON sa1.student_id = sa3.student_id AND sa3.date = DATE_ADD(sa2.date, INTERVAL 1 DAY)
+        WHERE sa1.status = 'late' AND sa2.status = 'late' AND sa3.status = 'late'
+    ) AS consecutive_issues
+    JOIN students s ON s.id = consecutive_issues.student_id
     GROUP BY s.id, s.name
-";
+"; 
 
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-       
-        echo 'Student ID: ' . $row["id"] . ' - Name: ' . $row["name"] . ' has been absent for 3 consecutive days.';
-     
+        echo 'Student ID: ' . $row["id"] . ' - Name: ' . $row["name"] . ' has been absent or late for 3 consecutive days.';
     }
-} else {
-    echo '<div class="notification">No students with 3 consecutive absences found.';
     
+} else {
+    echo '<No students with 3 consecutive absences or lateness found.';
 }
 
 $conn->close();
